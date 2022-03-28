0d7432128b146744d94be6b669003d514763973f
Rudra-00 <82320714+Rudra-00@users.noreply.github.com>
2022-03-28 19:43:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent("canvas");
+	createCanvas(650 , 400)
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
